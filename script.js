387274aeb0e08adec64a8a558ccb232361d540ef
Hi-manshu2301 <104667314+Hi-manshu2301@users.noreply.github.com>
2026-06-06 17:56:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+fucntion name(ms){
+	return new Promise((resolve)=>{
+		setTimeout(resolve,ms);
+	});
+}
+document.getElementById("btn").addEvenetListener("click",async function(){
+	let input = document.getElementById("input").value;
+	let delay = Number(document.getElementById("delay").value);
+	await name(delay);
+	document.getElementById("output").innerText = input;
+})
